70ad3185c5424be795d4047155f84adb4cb2158f
Andrew Fyfe <enumera2000@yahoo.co.uk>
2015-10-11 21:46:41 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yodleeapp::Application.config.secret_token = '7d50f1a60242550672614c68e6a6aba43f87fddaf8944a93dc9f6dc37d7645a2f68c9673b467c19602eed7a9662b23b8f53f95937e5f3a39d00b789507a38578'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Yodleeapp::Application.config.session_store :cookie_store, key: '_Yodleeapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Yodleeapp::Application.config.session_store :active_record_store
